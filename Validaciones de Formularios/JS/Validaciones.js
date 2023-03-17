8cbcc73cea009e0625aeccb301c7dcbfe9736768
“rodolfo.espinosa.mendez.contractor” <rodolfo.espinosa.mendez.contractor@bbva.com>
2023-03-17 17:51:29 -0600

    Metiendo Lógica a los errores en Formulario

--- Validaciones de Formularios/JS/Validaciones.js
+++ Validaciones de Formularios/JS/Validaciones.js
@@ -1,12 +1,56 @@
 export function valida(input){
     const tipoDeInput = input.dataset.tipo;
-    if(validadores[tipoDeInput]){
+    if (validadores[tipoDeInput]){
         validadores[tipoDeInput](input)
+    }
+
+    if (input.validity.valid){
+        input.parentElement.classList.remove("input-container--invalid");
+        input.parentElement.querySelector(".input-message-error").innerHTML = "";
+    }else {
+        input.parentElement.classList.add("input-container--invalid");
+        input.parentElement.querySelector(".input-message-error").innerHTML = mostrarMensajeDeError(tipoDeInput, input)
     }
 }
 
+const tipoDeErrores = [
+    "valueMissing",
+    "typeMismatch",
+    "patternMismatch",
+    "customError",
+]
+
+const mensajesDeError = {
+    nombre: {
+        valueMissing: "Este campo no puede estar vacío",
+    },
+    email: {
+        valueMissing: "Este campo no puede estar vacío",
+        typeMismatch: "El correo no es válido"
+    },
+    password: {
+        valueMissing: "Este campo no puede estar vacío",
+        patternMismatch: "Al menos 6 caracteres, debe contener al menos una minúscula, una mayúsculas, un número y un símbolo especial"
+    },
+    nacimiento: {
+        valueMissing: "Este campo no puede estar vacío",
+        customError: "Debes tener al menos 18 años de edad"
+    },
+};
+
 const validadores = {
-    nacimiento: input => validarNacimiento(input)
+    nacimiento: (input) => validarNacimiento(input),
+};
+
+function mostrarMensajeDeError(tipoDeInput, input){
+    let mensaje = "";
+    tipoDeErrores.forEach( error => {
+        if(input.validity[error]){
+            
+        }
+    })
+
+    return mensaje;
 }
 
 // const inputNacimiento = document.querySelector("#birth");
