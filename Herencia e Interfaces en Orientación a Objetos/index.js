161b56b1222a1846e28ad5821a3cfe06693d15d6
“rodolfo.espinosa.mendez.contractor” <rodolfo.espinosa.mendez.contractor@bbva.com>
2023-04-05 21:51:24 -0500

    replicando herencia y abstracción con empleados

--- Herencia e Interfaces en Orientación a Objetos/index.js
+++ Herencia e Interfaces en Orientación a Objetos/index.js
@@ -1,9 +1,15 @@
 /*Importación de clases*/
 import {Cliente} from './Cliente.js'
-import { Cuenta } from './Cuenta.js';
-import { CuentaAhorro } from './CuentaAhorro.js';
-import { CuentaCorriente } from './CuentaCorriente.js';
+import { Cuenta } from './Cuentas/Cuenta.js';
+import { CuentaAhorro } from './Cuentas/CuentaAhorro.js'
+import { CuentaCorriente } from './Cuentas/CuentaCorriente.js';
+import { CuentaNomina } from './Cuentas/CuentaNomina.js';
+import { Empleado } from './Empleados/Empleado.js';
+import { Gerente } from './Empleados/Gerente.js';
+import { Director } from './Empleados/Director.js';
 
+// Referencias a Cuenta. Se comentan para probar la parte de Empleado
+/*
 const cliente = new Cliente('Leonardo','13804050','123224');
 const cliente2 = new Cliente('María','16979808','8989');
 
@@ -11,6 +17,7 @@ const cuentaDeLeonardo = new CuentaCorriente(cliente, '1', '001');
 const cuentaDeMaria = new CuentaCorriente(cliente2,'2','002');
 
 const cuentaAhorroLeonardo = new CuentaAhorro(cliente, "99985", "001", 0)
+*/
 
 /*
 console.log(cuentaDeLeonardo);
@@ -28,5 +35,16 @@ console.log(cuentaAhorroLeonardo.verSaldo());
 cuentaAhorroLeonardo.prueba();// Esta es una prueba para comprobar la sobre escritura de un método padre.
 */
 
+/*
 const cuentaSimple = new Cuenta(cliente, "23", "099") // aquí estamos haciendo uso directo de la clase padre "Cuenta", lo cual no es correcto.
 console.log(cuentaSimple);
+*/
+
+// Referencias a Empleado. Se comentan para probar la parte de Cuenta
+const empleado = new Empleado("Juan Perez", "12121212", 10000);
+const gerente = new Gerente("Helena Moreno", "92929", 12000);
+const director = new Director("Raúl Jimenez", "92929203", 15000);
+
+console.log(empleado.verBonificacion());
+console.log(gerente.verBonificacion());
+console.log(director.verBonificacion());
