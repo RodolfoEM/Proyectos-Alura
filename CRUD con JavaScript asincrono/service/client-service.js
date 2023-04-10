b5299520884c5c797cbe019c7204762f8945f62a
“rodolfo.espinosa.mendez.contractor” <rodolfo.espinosa.mendez.contractor@bbva.com>
2023-04-09 21:17:52 -0500

    Inmplementando el uso de Fetch API

--- CRUD con JavaScript asincrono/service/client-service.js
+++ CRUD con JavaScript asincrono/service/client-service.js
@@ -30,32 +30,25 @@ const crearNuevaLinea = (nombre, email) => {
 
 const table = document.querySelector("[data-table]"); // Cramos una variable en donde guardaremos la etiqueta que tenga como referencia data-table
 
-const listaClientes = () => {
-  const promise = new Promise((resolve, reject) => {
-    const http = new XMLHttpRequest();
-    //CRUD: Create-POST, Read-GET, Update-PUT/PATCH, Delete-DELETE
-    // Abrir-open http(recibe el método y la url). Con esto dejamos que JS se encargue de obtener la información
-    http.open("GET", "http://localhost:3000/perfil");
+//Fetch API
+const listaClientes = () =>
+  // El código de un arrow function puede omitir los return si omitiumos las llaves, como en este ejemplo.
+  fetch("http://localhost:3000/perfil") //Fetch es una función que sirve para realizar peticiones y obtener respues de http. Recibe como parámetro el tipo de solicitud y la URL(en caso de no venir el tipo de solicitud, se asume GET).
+    .then(
+      (
+        respuesta // Seguido colocamos el .then en donde colocaremos la función con la indicación de lo que queremos que se haga con la respuesta
+      ) => respuesta.json() // Aquí le damos un formato de JSON a la respuesta
+    );
 
-    http.send(); //Esta funcion se encarga de enviar la petición
-
-    http.onload = () => {
-      //Aquí obtenemos la respuesta del servidor.
-      const response = JSON.parse(http.response); // aquí convertimos en un objeto el texto que se nos envía como respuesta
-      if (http.status >= 400) { // Aqupi validamos el código de respuesta del http, en caso de que sea mayopr o igual a 400 se entiende que hubo un error y se procede con el reject, en caso de que no se envía la respuesta.
-        reject(response);
-      } else {
-        resolve(response);
-      };
-    };
-  });
-  return promise;
-};
-
-listaClientes().then((data) => { //Con esta función manejamos lo que sucederá en caso de obtener una respuesta de nmuestra promesa. La información de respuesta de la promesa la recibimos con el parámetro "data"
+listaClientes()
+  .then((data) => {
+    //Con esta función manejamos lo que sucederá en caso de obtener una respuesta de nmuestra promesa. La información de respuesta de la promesa la recibimos con el parámetro "data"
     data.forEach((perfil) => {
       // Aquí estamos iterando entre cada uno de los datos u objetos que se nos envía como respuesta.
       const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email); // Aquí creamos una constante que gusrdará el resultado de ejecutar la finción nueva línea, en dónde se recibe el nombre e email y lo guarda en un formato para html
       table.appendChild(nuevaLinea); // Aquí insertamos en la etiqueta padre de la tabla el html con la nueva información del usuario
     });
-}).catch((error) => { alert("Ocurrió un error")}); // Agregamos un catch para manejar el error en caso de que suceda.
+  })
+  .catch((error) => {
+    alert("Ocurrió un error");
+  }); // Agregamos un catch para manejar el error en caso de que suceda.
