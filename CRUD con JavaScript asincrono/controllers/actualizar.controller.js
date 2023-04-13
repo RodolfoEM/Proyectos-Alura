08b2e2112b91c39d3827f87c7fc94411cff72365
“rodolfo.espinosa.mendez.contractor” <rodolfo.espinosa.mendez.contractor@bbva.com>
2023-04-13 17:13:11 -0500

    implementación de async y await / try catch

--- CRUD con JavaScript asincrono/controllers/actualizar.controller.js
+++ CRUD con JavaScript asincrono/controllers/actualizar.controller.js
@@ -2,7 +2,8 @@ import { clientServices } from "../service/client-service.js";
 
 const formulario = document.querySelector("[data-form]");
 
-const obtenerInformacion = () => {
+
+const obtenerInformacion = async () => {
     const url = new URL(window.location); // esta es una clase(URL) que ya viene instalada en JS
     const id = url.searchParams.get("id");
 
@@ -12,10 +13,25 @@ const obtenerInformacion = () => {
     const nombre = document.querySelector("[data-nombre]")
     const email = document.querySelector("[data-email]")
 
-    clientServices.detalleCliente(id).then( perfil => {
-        nombre.value = perfil.nombre;
-        email.value = perfil.email;
-    })
+    try { // try cath se usa dentro de las funciones asincronas para el manejo de errores o excepciones
+        const perfil = await clientServices.detalleCliente(id);
+        if(perfil.nombre && perfil.email){
+            nombre.value = perfil.nombre;
+            email.value = perfil.email;
+        }else{
+            throw new Error(); // Aquí hacemos llamado al catch en caso de que perfil(nombre e email) este vacío
+        }
+    } catch (error) {
+        console.log("Catch error - ", error);
+        alert("Hubo un error")
+        window.location.href = "/screens/error.html"
+    }
+
+    //Este código se comenta ya que con el uso de async y await podemos manejarlo de otra forma, ejemplo: se crea la constante perfil en donde recibimos la respuesta, por lo que omitimos el uso de .then
+    // clientServices.detalleCliente(id).then( perfil => {
+    //     nombre.value = perfil.nombre;
+    //     email.value = perfil.email;
+    // })
 }
 
 formulario.addEventListener("submit", (evento) => {
