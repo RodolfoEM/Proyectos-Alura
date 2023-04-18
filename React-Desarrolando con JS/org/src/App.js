a52aee3efce51b916e645dfb7df4f118feb08e21
“rodolfo.espinosa.mendez.contractor” <rodolfo.espinosa.mendez.contractor@bbva.com>
2023-04-17 23:59:10 -0500

    creando y estilo al componente Form(formulario)

--- React-Desarrolando con JS/org/src/App.js
+++ React-Desarrolando con JS/org/src/App.js
@@ -1,28 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Form from './componentes/Form/Form';
 import Header from './componentes/Header/Header.js';
-console.log(Header);
+
 
 function App() {
   return (
-    <div className="App">
-    {/* {Header()} */} {/*Esta es otra forma de insertar un componente  */}
-    {/* <Header></Header> Esta es otra forma de insertar un componente  */}
-    <Header/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola Mundo con React!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprende React
-        </a>
-      </header>
+    <div>
+      {/* {Header()} */} {/*Esta es otra forma de insertar un componente  */}
+      {/* <Header></Header> Esta es otra forma de insertar un componente  */}
+      <Header/>
+      <Form />
     </div>
   );
 }
