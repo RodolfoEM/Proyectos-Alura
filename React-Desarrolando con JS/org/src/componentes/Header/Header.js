bbd953fd84c8192c93af7118792af2ba470ecbcc
“rodolfo.espinosa.mendez.contractor” <rodolfo.espinosa.mendez.contractor@bbva.com>
2023-04-17 23:32:33 -0500

    Creando un componente(Header)

---
+++ React-Desarrolando con JS/org/src/componentes/Header/Header.js
@@ -0,0 +1,9 @@
+import "./Header.css"
+
+function Header(){
+    return  <header className="header"> {/* classNAme es la forma de colocar una clase dentro de una etiqueta HTML, se evita el uso de class solo, debido a la palabra reservada en la POO */}
+        <img src="/img/HeaderHeader_total.png" alt="Org" />
+    </header>
+}
+
+export default Header
