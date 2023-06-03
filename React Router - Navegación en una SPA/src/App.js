e82ef34c388492d2fcc38e6c3c78b367eb49a856
“rodolfo.espinosa.mendez.contractor” <rodolfo.espinosa.mendez.contractor@bbva.com>
2023-06-03 12:40:25 -0500

    Agregando Header a las secciones y default 404

--- React Router - Navegación en una SPA/src/App.js
+++ React Router - Navegación en una SPA/src/App.js
@@ -2,6 +2,9 @@ import './assets/css/base/base.css';
 import './assets/css/componentes/card.css'
 import Home from "./pages/Home"
 import Sobre from "./pages/Sobre"
+import Page404 from './pages/Pages404'
+import Header from './components/Header';
+
 
 //Aquí importamos la librería "react-router-dom" y la usamos para iterar entre las diferentes rutas de nuestra web
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
@@ -12,9 +15,11 @@ function App() {
     <>
     {/* Aquí usamos la librería "react-router-dom" */}
     <Router>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/sobre' element={<Sobre />} />
+        <Route path='*' element={<Page404 />} />
       </Routes>
     </Router>
     </>
