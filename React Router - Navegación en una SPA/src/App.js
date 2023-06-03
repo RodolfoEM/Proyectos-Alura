2e9532e1cbb1dd168930eaa5453b885b3463b030
“rodolfo.espinosa.mendez.contractor” <rodolfo.espinosa.mendez.contractor@bbva.com>
2023-06-03 12:04:45 -0500

    Aplicando la librería "react-router-dom"

--- React Router - Navegación en una SPA/src/App.js
+++ React Router - Navegación en una SPA/src/App.js
@@ -1,10 +1,22 @@
 import './assets/css/base/base.css';
 import './assets/css/componentes/card.css'
+import Home from "./pages/Home"
+import Sobre from "./pages/Sobre"
+
+//Aquí importamos la librería "react-router-dom" y la usamos para iterar entre las diferentes rutas de nuestra web
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 function App() {
+
   return (
     <>
-
+    {/* Aquí usamos la librería "react-router-dom" */}
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sobre' element={<Sobre />} />
+      </Routes>
+    </Router>
     </>
   );
 }
