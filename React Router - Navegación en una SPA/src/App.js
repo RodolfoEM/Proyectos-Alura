ed27c3efb9eb1a1c7efafc21ca20151ca2073104
“rodolfo.espinosa.mendez.contractor” <rodolfo.espinosa.mendez.contractor@bbva.com>
2023-06-05 11:29:59 -0500

    incluyendo filtros por categoróia y sub-categoria

--- React Router - Navegación en una SPA/src/App.js
+++ React Router - Navegación en una SPA/src/App.js
@@ -4,6 +4,8 @@ import Home from "./pages/Home"
 import Sobre from "./pages/Sobre"
 import Page404 from './pages/Pages404'
 import Header from './components/Header';
+import Post from './pages/Pots';
+import Categoria from './pages/Categoria';
 
 
 //Aquí importamos la librería "react-router-dom" y la usamos para iterar entre las diferentes rutas de nuestra web
@@ -19,6 +21,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/sobre' element={<Sobre />} />
+        <Route path='/posts/:id' element={<Post />} />
+        <Route path='/categoria/:id/*' element={<Categoria />} />
         <Route path='*' element={<Page404 />} />
       </Routes>
     </Router>
