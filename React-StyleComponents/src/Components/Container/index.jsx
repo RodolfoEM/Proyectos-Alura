784cfc640bc7f80f976964b87bb59b1f9ccca1b2
Rododev <mctot_2008@hotmail.com>
2023-06-08 18:59:17 -0600

    Componente para revision de datos de estado de cta

--- React-StyleComponents/src/Components/Container/index.jsx
+++ React-StyleComponents/src/Components/Container/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import Title from "../Title";
 import Account from "../Account";
+import List from "../List";
 
 const StyleContainer = styled.div `
   background-color: #f1f1f1;
@@ -23,7 +24,7 @@ const Container = () => {
       <Title>Smart Bank</Title>
       <StyleContent>
         <Account />
-        <Account />
+        <List />
       </StyleContent>
     </StyleContainer>
   );
