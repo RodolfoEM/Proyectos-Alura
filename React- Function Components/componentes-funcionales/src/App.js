14d2ae93593340a7e5aecd56fd7f70172bc8cc99
Rododev <mctot_2008@hotmail.com>
2023-06-11 12:37:24 -0600

    ++ fuente,  Switch y container de material UI

--- React- Function Components/componentes-funcionales/src/App.js
+++ React- Function Components/componentes-funcionales/src/App.js
@@ -4,13 +4,16 @@
 import logo from './logo.svg';
 import './App.css';
 import FormSignUp from './components/FormSignUp';
+import { Container } from '@mui/material';
+import Typography from '@mui/material/Typography';
+
 
 function App() {
   return (
-    <>
-      <h1>Formulario Registro</h1>
+    <Container component="section" maxWidth="sm">
+      <Typography variant="h3" align="center" component="h1" >Formulario Registro</Typography>
       <FormSignUp />
-    </>
+    </Container>
   );
 }
 
