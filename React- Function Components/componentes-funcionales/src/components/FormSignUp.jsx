3b58898f5de7aea2c50c3c4e5a66e25fe88f4324
Rododev <mctot_2008@hotmail.com>
2023-06-10 23:40:08 -0600

    Usando function components vs class components

--- React- Function Components/componentes-funcionales/src/components/FormSignUp.jsx
+++ React- Function Components/componentes-funcionales/src/components/FormSignUp.jsx
@@ -1,8 +1,35 @@
+import Button from "@mui/material/Button";
+import TextField from '@mui/material/TextField';
+
 function FormSignUp() {
-    return <form>
-        <input type="text" name="" id="" />
-        <button>Registrarse</button>
+    return(
+    <form>
+        <TextField
+            id="name"
+            label="Nombre"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            />
+        <TextField
+            id="lastName"
+            label="Apellids"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            />
+        <TextField
+            id="email"
+            label="Email"
+            ariant="outlined"
+            fullWidth
+            margin="normal"
+            />
+        <label>Promociones</label>
+        <input type="checkbox"/>
+        <Button variant="contained" >Registrarse</Button>
     </form>
+    )
 }
 
 export default FormSignUp;
